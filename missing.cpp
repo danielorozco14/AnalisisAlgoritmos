1281efde1f7864db1b912e260c31bb711712c14d
danielorozco14 <37311558+danielorozco14@users.noreply.github.com>
2019-09-17 14:45:05 -0600

    Tarea Labo 4

--- missing.cpp
+++ missing.cpp
@@ -2,16 +2,13 @@
 
 using namespace std;
 
-int partition (int arr[], int p, int r)
-{
+int partition (int arr[], int p, int r){
 	int i, j, x, tmp;
 	
 	x = arr[r];
 	i = p - 1;
-	for (j = p; j < r; j++)
-	{
-		if (arr[j] <= x)
-		{
+	for (j = p; j < r; j++)	{
+		if (arr[j] <= x){
 			i++;
 			
 			tmp = arr[i];
@@ -27,39 +24,32 @@ int partition (int arr[], int p, int r)
 	return i;
 }
  
-void quicksort (int arr[], int p, int r)
-{
+void quicksort (int arr[], int p, int r){
 	int q;
 	
-	if (p < r)
-	{
+	if (p < r){
 		q = partition (arr, p, r);
 		quicksort (arr, p, q - 1);
 		quicksort (arr, q + 1, r);
 	}
 }
  
-int main ()
-{
+int main (){
 	int  t, n, p,data;
 	
 	cin>>t;
-	for (int i = 0; i < t; i++)
-	{
+	for (int i = 0; i < t; i++){
 		cin>>n;
 		int arr [n];
-		for (int j = 0; j < n; j++)
-		{
-            cin>>data;
+		for (int j = 0; j < n; j++){
+            		cin>>data;
 			arr[j]=data;
 		}
 		quicksort(arr, 0, n - 1);
 		
 		p = 2;
-		for (int x = 0; x < n; x++)
-		{
-			if (arr[x] >= p)
-			{
+		for (int x = 0; x < n; x++){
+			if (arr[x] >= p){
 				p += 2;
 			}
 		}
